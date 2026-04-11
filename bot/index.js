e185fdc1c8c6bd57414ed04c932e5e82530dc651
regiwarosu <hujiotensai@gmail.com>
2026-04-11 12:12:59 +0900

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -1,4 +1,13 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(3000);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
