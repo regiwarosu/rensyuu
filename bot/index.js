1918ea98a662ed7d31094a5b50fe840decf0963a
regiwarosu <hujiotensai@gmail.com>
2026-04-11 11:23:01 +0900

    Initialize Discord bot client with login

---
+++ bot/index.js
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
